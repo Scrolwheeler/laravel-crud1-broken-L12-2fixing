386802c492fde31769f027a7808aa78d097a3382
Your Name <you@example.com>
2026-03-31 14:40:09 +0300

    Forma nesavienojas ar database, no database VARievadīt informaciju kura tiek izvadita

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,7 +4,7 @@
     <input type="submit" value="Dzēst">
 </form>
 
-<h2>{{ title }}</h2>
-<h3>{{ author }}</h3>
-<p>{{ released_at }}</p>
+<h2>{{ $title->title }}</h2>
+<h3>{{ $author->author }}</h3>
+<p>{{ $released_at->released_at }}</p>
 <a href="/books">All books</a>
